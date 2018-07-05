07320e7e700aec3b64f5c32c097c2b2f6c743d97
Jesse Barkdoll <jessebarkdoll@gmail.com>
2018-07-05 14:54:19 -0400

    use os.walk() for random selections

--- video_raffle.py
+++ video_raffle.py
@@ -15,6 +15,13 @@ DATA_DIRS = [ 'put all of your', 'video file paths', 'in this list' ]
 MEDIA_PLAYER = 'C:\\Program Files\\Change\\This\\Path\\to\\yourMediaPlayer.exe'
 
 
+def isVideo(f):
+	if f.endswith(('.mp4', '.mkv', '.avi', '.flv')):
+		return True
+	else:
+		return False
+
+
 def playerRunning(player):
 	player_process = player.split('\\')[-1]
 	yes = False
@@ -24,43 +31,48 @@ def playerRunning(player):
 	return yes
 
 
-def generateSelectionList(data_path_list):
+def generateDirList(data_path_list):
 	dir_list = []
 	for root_folder in data_path_list:
-		dir_list += [ os.path.join(root_folder, d) for d in os.listdir(root_folder)
-			if Path(root_folder+d).is_dir() ]
+		dir_list += [os.path.join(root_folder, d) for d in os.listdir(root_folder)
+                    if Path(root_folder+d).is_dir()]
 	return dir_list
 
 
+def generateSelectionList(data_path_list):
+	file_list = []
+	for root_folder in data_path_list:
+		for root, directory, files in os.walk(root_folder):
+			for fname in files:
+				fpath = root + '\\' + fname
+				if isVideo(fpath):
+					file_list.append(fpath)
+
+	return file_list
+
+
 def chooseOne(path_list):
-	while True:
-		folders = generateSelectionList(path_list)
-		# Choose random folder to get video from
-		chosen_folder = random.choice(folders)
-		# Choose random file from selected folder
-		chosen_one = random.choice(os.listdir(chosen_folder))
-		# Handles any non-video file choices
-		if chosen_one.endswith(('.mp4', '.mkv', '.avi', '.flv')):
-			break
-	
-	chosen_path = chosen_folder + '\\' + chosen_one
-	return chosen_path
-	
+	files = generateSelectionList(path_list)
+	# Choose random folder to get video from
+	chosen_one = random.choice(files)
+	# Choose random file from selected folder
+	return chosen_one
+
 
 def chooseSeries(paths):
-	# Use os.walk() or similar operation to
-	# grab all media files within the given folder (or subfolder)
-	# and open them in VLC
+	# 01 chosen_folder = random.choice(generateDirList())
+	# 02 use os.walk() or similar operation to
+	# grab all media files within the chosen folder's
+	# files and subfolders
+	# 03 Generate an alphabetically sorted list
+	# of the video files and open them as a playlist in VLC
 	pass
 
 
-try: 
+try:
 	# Make thecall!
 	p = chooseOne(DATA_DIRS)
-	while not Path(p).is_file():
-		p = chooseOne(DATA_DIRS)
-
 	print('\nplaying {}\n'.format(p))
-	# subprocess.Popen([MEDIA_PLAYER, p])
+	subprocess.run([MEDIA_PLAYER, p])
 except Exception as e:
 	print(e)
